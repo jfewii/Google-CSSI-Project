d654b154a279b27fac92a3d4be403811f87519d4
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 16:34:44 -0700

    added login

--- materialize/main.py
+++ materialize/main.py
@@ -1,4 +1,5 @@
 import webapp2
+import jinja2
 from google.appengine.api import users
 from model import CssiUser
 
--- materialize/model.py
+++ materialize/model.py
@@ -1,8 +1,8 @@
-import webapp2
-
-from google.appengine.ext import ndb
-
-class CssiUser(ndb.Model):
-  first_name = ndb.StringProperty()
-  last_name = ndb.StringProperty()
-  email = ndb.StringProperty()
+# import webapp2
+#
+# from google.appengine.ext import ndb
+#
+# class CssiUser(ndb.Model):
+#   first_name = ndb.StringProperty()
+#   last_name = ndb.StringProperty()
+#   email = ndb.StringProperty()
