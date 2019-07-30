9c5871dc554186b9f5282dc1a81cd2208566141e
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-30 09:25:50 -0700

    started profile data store

--- materialize/model.py
+++ materialize/model.py
@@ -12,3 +12,22 @@ from google.appengine.ext import ndb
 class UserDataStore(ndb.Model):
     username = ndb.StringProperty()
     psw = ndb.StringProperty()
+
+class ProfileStore(ndb.Model):
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    username = ndb.StringProperty()
+    password = ndb.StringProperty()
+    age = ndb.StringProperty()
+    gender = ndb.StringProperty()
+
+
+    industry = ndb.StringProperty()
+    income = ndb.StringProperty()
+    sex_orient = ndb.StringProperty()
+    city = ndb.StringProperty()
+
+    zip_code = ndb.StringProperty()
+
+    def get_profile_info(self):
+        return "hello " + self.first_name + " " + self.last_name
