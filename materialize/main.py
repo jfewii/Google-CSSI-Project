f4d1d93f77c8cea4427032a98346052154a38e39
cssi <cssi@cssis-MacBook-Pro.local>
2019-08-01 11:08:21 -0700

    fixed messages

--- materialize/main.py
+++ materialize/main.py
@@ -216,9 +216,9 @@ class ProfilePage(webapp2.RequestHandler):
             self.response.write(profile_template.render(profilelog))
 
 
-class FriendsPage(webapp2.RequestHandler):
+class MessagePage(webapp2.RequestHandler):
     def get(self):
-        friends_template = the_jinja_env.get_template('/friends.html')
+        friends_template = the_jinja_env.get_template('/messages.html')
         status_query = MessageDataStore.query().order(-MessageDataStore.StatusTime)
         messagecollection = status_query.fetch()
 
@@ -228,7 +228,7 @@ class FriendsPage(webapp2.RequestHandler):
         self.response.write(friends_template.render(the_variable_dict))
 
     def post(self):
-        friends_template = the_jinja_env.get_template('/friends.html')
+        friends_template = the_jinja_env.get_template('/messages.html')
         user = users.get_current_user()
         nickname = user.nickname()
         status = self.request.get("CurrentStatus")
@@ -237,11 +237,11 @@ class FriendsPage(webapp2.RequestHandler):
         messagestore.put()
         time.sleep(0.1)
 
-        self.redirect('/friends')
+        self.redirect('/messages')
 
-class MessagesPage(webapp2.RequestHandler):
+class FriendsPage(webapp2.RequestHandler):
     def get(self):
-        messages_template = the_jinja_env.get_template('/messages.html')
+        messages_template = the_jinja_env.get_template('/friends.html')
         self.response.write(messages_template.render())
 
 class AboutPage(webapp2.RequestHandler):
